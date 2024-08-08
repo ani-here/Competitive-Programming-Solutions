eacd63ecacee5f41c926907f1cd3e409f1fe92c7
ani-here <140171085+ani-here@users.noreply.github.com>
2024-08-08 10:34:35 +0530

    Create A+B.py

---
+++ Codeforces/Codeforces Round 964 (Div. 4)/A+B.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from collections import deque
+from itertools import permutations
+from math import *
+
+ 
+import sys
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+tmii = lambda: tuple(map(int, input().split()))
+mfi = lambda: map(float, input().split())
+lmfi = lambda: list(map(float, input().split()))
+
+T = ii()
+ct = 0
+
+
+def solve():
+    n = input()
+    print(int(n[0])+int(n[1]))
+
+
+
+#solve()  
+for _ in range(T):
+    solve() 
